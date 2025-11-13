caa33e42defce330a7eabf433b18df745b1d28ac
Alibasha <shaikallibasha8@gmail.com>
2025-11-13 14:22:19 +0000

    main added

--- src/main.py
+++ src/main.py
@@ -10,9 +10,18 @@ def mul(a,b):
 
 def div(a,b):
     return a/b
+<<<<<<< Updated upstream
 
 print(add(10,20))
 print(sub(10,5))
 print(mul(10,5))
 print(div(10,5))
+
+if __name__ == "__main__":
+    print(add(10,20))
+    print(sub(10,5))
+    print(mul(10,5))
+    print(div(10,5))
+    
+
 
